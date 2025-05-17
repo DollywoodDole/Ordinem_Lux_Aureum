e0820daa86840e55e9133ba62c1ba94295d5c31b
DollywoodDole <daltonellscheid@gmail.com>
2025-05-17 13:49:04 -0600

    Good

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Fallback to index.html for all routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
